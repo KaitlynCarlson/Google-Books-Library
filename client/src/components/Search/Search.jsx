205b8c1f2b5b061fc595fb82c1a44aa798136775
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-21 15:59:56 -0700

    Minor Styling

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import "./style.css";
 function Search(props) {
   return (
-    <form id="discoverForm">
-      <div className="form-group">
-        <label for="bookSearch">Uncharted worlds and words await you</label>
-        <input
-          type="text"
-          className="form-control"
-          id="bookSearch"
-          {...props}
-          placeholder="Book Title"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    <div className="jumbotron jumbotron-fluid" id="jumboSearchAndResults">
+      <form id="discoverForm">
+        <div className="form-group">
+          <label for="bookSearch">Uncharted worlds and words await you</label>
+          <input
+            type="text"
+            className="form-control"
+            id="bookSearch"
+            {...props}
+            placeholder="Book Title"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 export default Search;
